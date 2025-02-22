ea82ae818d32b45b6cc08c12d8f419f03727506f
BINDU-N-R <bindu6433@gmail.com>
2025-02-22 13:14:05 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import ReactHookForm from "./components/reactHookForm";
+// import ReactHookFormWithZod from "./components/reactHookFormWithZod";
+// import SimpleForm from "./components/simpleForm";
+
+function App() {
+  return (
+    <div>
+      <h1>React Hook Form Instance</h1>
+      {/* <ReactHookFormWithZod /> */}
+      <ReactHookForm/>
+      {/* <SimpleForm/> */}
+    </div>
+  );
+}
+
+export default App;
